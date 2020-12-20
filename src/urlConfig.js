140e365a1709d7a8ff8a339f5b42ff3140ff307e
Ankit3110200 <chotaliyaankit31@gmail.com>
2020-12-20 19:30:20 +0530

    Ankit-2 commit

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,2 +1,2 @@
-export const api = 'http://localhost:7000/api'
-export const generatepublicimgUrl=(filename)=>{ return `http://localhost:7000/public/${filename}`}
+export const api = 'https://mernbackendankit.herokuapp.com/api'
+export const generatepublicimgUrl=(filename)=>{ return `https://mernbackendankit.herokuapp.com/public/${filename}`}
